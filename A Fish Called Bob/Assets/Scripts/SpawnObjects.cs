377b996d5f190f9c3427561b9b2590a55d00259e
Lottie Bryant <amaliciabryant@gmail.com>
2020-05-16 15:21:34 +0100

    WIP spawning at intervals

--- A Fish Called Bob/Assets/Scripts/SpawnObjects.cs
+++ A Fish Called Bob/Assets/Scripts/SpawnObjects.cs
@@ -5,11 +5,11 @@ using UnityEngine;
 public class SpawnObjects : MonoBehaviour
 {
     public GameObject fishingSpot;
+    int numFishSpots = Random.Range(1, 10);
 
     void Start()
     {
         Vector3 center = transform.position;
-        int numFishSpots = Random.Range(1, 5);
 
         for (int i = 0; i < numFishSpots; i++)
         {
@@ -18,6 +18,8 @@ public class SpawnObjects : MonoBehaviour
             var fishSpot = Instantiate(fishingSpot, pos, Quaternion.identity);
             fishSpot.transform.parent = transform;
         }
+
+        //InvokeRepeating("SpawnMore", 10.0f, 10.0f);
     }
 
     Vector3 RandomCircle(Vector3 center, float radius)
@@ -28,5 +30,18 @@ public class SpawnObjects : MonoBehaviour
         pos.y = center.y;
         pos.z = center.z + radius * Mathf.Sin(ang * Mathf.Deg2Rad);
         return pos;
+    }
+
+    void SpawnMore()
+    {
+        Vector3 center = transform.position;
+        int chance = Random.Range(1, 100);
+
+        if (chance == 100)
+        {
+            Vector3 pos = RandomCircle(center, 100f);
+            var fishSpot = Instantiate(fishingSpot, pos, Quaternion.identity);
+            fishSpot.transform.parent = transform;
+        }
     }
 }
