f4c69db37f526c9759fd2adf74bb89ae8f7902f0
Lottie Bryant <amaliciabryant@gmail.com>
2020-05-16 22:03:33 +0100

    WIP collisions

--- A Fish Called Bob/Assets/Scripts/Bob.cs
+++ A Fish Called Bob/Assets/Scripts/Bob.cs
@@ -4,6 +4,8 @@ using UnityEngine;
 
 public class Bob : MonoBehaviour
 {
+    public SpawnFishItem obejctSpawner;
+    public GameObject player;
     // Start is called before the first frame update
     void Start()
     {
@@ -14,5 +16,23 @@ public class Bob : MonoBehaviour
     void Update()
     {
         
+    }
+
+    void OnCollisionEnter(Collision collision)
+    {
+        Debug.Log("Collision");
+    }
+
+    void OnCollisionStay(Collision collision)
+    {
+        if (collision.gameObject.tag == "FishingSpot")
+        {
+            Debug.Log("Collision between bob and fishing spot");
+            int time = Random.Range(1, 10);
+            //yield return new WaitForSeconds(time);
+            Vector3 spawnPos = player.transform.position;
+            spawnPos.y += 1.0f;
+            obejctSpawner.CatchFish(spawnPos);
+        }
     }
 }
