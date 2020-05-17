56ac6b7ed6deeed924780f06f07c7bc1bede787e
Lottie Bryant <amaliciabryant@gmail.com>
2020-05-17 15:27:17 +0100

    WIP rocks

--- A Fish Called Bob/Assets/Scripts/SpawnObjects.cs
+++ A Fish Called Bob/Assets/Scripts/SpawnObjects.cs
@@ -5,22 +5,29 @@ using UnityEngine;
 public class SpawnObjects : MonoBehaviour
 {
     public GameObject fishingSpot;
-
-    private float nextActionTime = 10f;
-    private float period = 10f;
+    public GameObject rock;
 
     void Start()
     {
         int numFishSpots = Random.Range(1, 10);
+        int numRocks = Random.Range(1, 10);
 
         Vector3 center = transform.position;
 
         for (int i = 0; i < numFishSpots; i++)
         {
             Vector3 pos = RandomCircle(center, 100f);
-            //Quaternion rot = Random.rotation;
             var fishSpot = Instantiate(fishingSpot, pos, Quaternion.identity);
             fishSpot.transform.parent = transform;
+        }
+
+        for (int i = 0; i < numRocks; i++)
+        {
+            Vector3 pos = RandomCircle(center, 100f);
+            var rocks = Instantiate(rock, pos, Quaternion.Euler(new Vector3(Random.Range(0, 360), Random.Range(0, 360), Random.Range(0, 360))));
+            //rocks.transform.rotation = Random.rotation;
+            //rocks.transform.eulerAngles = new Vector3(Random.Range(-100, 100), Random.Range(-100, 100), Random.Range(-100, 100));
+            rocks.transform.parent = transform;
         }
 
         InvokeRepeating("SpawnMore", 10.0f, 10.0f);
