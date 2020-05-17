20e0fe48bfe94d8dc8df99ed7be0ec13f34549a6
Lottie Bryant <amaliciabryant@gmail.com>
2020-05-17 16:40:41 +0100

    Working selling of fish and other objects

---
+++ A Fish Called Bob/Assets/Scripts/SellFish.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SellFish : MonoBehaviour
+{
+	public int price;
+
+	void OnTriggerEnter(Collider other)
+	{
+		Debug.Log("Triggered");
+		if (other.gameObject.tag == "Seller")
+		{
+			TotalMoney.total += price;
+			Debug.Log(TotalMoney.total);
+			Destroy(gameObject);
+		}
+	}
+}
