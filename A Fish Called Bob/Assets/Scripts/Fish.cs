faa070e1ff12e35146bf243dbc19e95fcd5255de
Mikymac <mikymacmc@hotmail.co.uk>
2020-05-17 16:26:58 +0100

    Fishing distance thing done

--- A Fish Called Bob/Assets/Scripts/Fish.cs
+++ A Fish Called Bob/Assets/Scripts/Fish.cs
@@ -4,6 +4,10 @@ using UnityEngine;
 
 public class Fish : MonoBehaviour
 {
+    public bool Increasing = true, Relaseed = false;
+    public GameObject aim;
+    public float dist = 5, min = 5, max = 15, castSpeed = 1;
+
     private LineRenderer lineRenderer;
     public GameObject maxLineEndpoint;
     public GameObject bob;
@@ -19,15 +23,36 @@ public class Fish : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        if (Input.GetKeyDown("space") && !isCast)
+        if (Input.GetKey("space") && !isCast)
+        {
+            if (Increasing)
+            {
+                dist += Time.deltaTime * castSpeed;
+                if (dist >= max)
+                    Increasing = false;
+            }
+            else
+            {
+                dist -= Time.deltaTime * castSpeed;
+                if (dist <= min)
+                    Increasing = true;
+            }
+
+            aim.transform.position = transform.position + (transform.TransformDirection(Vector3.forward) * dist);
+            aim.transform.position = new Vector3(aim.transform.position.x, 1, aim.transform.position.z);
+        }
+
+
+        if (Input.GetKeyUp("space") && !isCast)
         {
             isCast = true;
-            Vector3 maxLineEnd = maxLineEndpoint.transform.position;
+            Vector3 maxLineEnd = aim.transform.position; // maxLineEndpoint.transform.position;
             maxLineEnd.y = 0.1f;
 
             bob.transform.position = maxLineEnd;
             lineRenderer.SetPosition(0, transform.position);
             lineRenderer.SetPosition(1, maxLineEnd);
+            aim.transform.position = new Vector3(0, -5, 0);
             return;
         }
 
