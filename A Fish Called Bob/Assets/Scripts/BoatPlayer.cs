f113ed9040278ac40847a8f1e512669d053e821d
Mikymac <mikymacmc@hotmail.co.uk>
2020-05-17 02:51:19 +0100

    Boat controls Finished

---
+++ A Fish Called Bob/Assets/Scripts/BoatPlayer.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BoatPlayer : MonoBehaviour
+{
+    public GameObject Player, Boat;
+    public bool controlBoat = false;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(controlBoat)
+        {
+            Player.SetActive(false);
+            Boat.SetActive(true);
+        }
+        else
+        {
+            Player.SetActive(true);
+            Boat.SetActive(false);
+        }
+    }
+}
